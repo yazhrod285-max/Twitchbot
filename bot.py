044a7efdcfe001bae71b118fb905cdd55f245519
yazhrod285-max <yazhrod285@gmail.com>
2026-05-03 21:34:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,20 @@ import os
 from twitchio.ext import commands
 from deep_translator import GoogleTranslator
 
-# Variables Railway
+# Récupération du token
 TOKEN = os.getenv("TOKEN")
-CHANNEL = os.getenv("CHANNEL")
 
-# DEBUG (ça va afficher dans Railway)
+# Récupération des chaînes (séparées par des virgules)
+CHANNELS = os.getenv("CHANNELS")
+
+# Sécurité si jamais la variable est vide
+if CHANNELS:
+    CHANNELS = CHANNELS.split(",")
+else:
+    CHANNELS = []
+
 print("TOKEN =", TOKEN)
-print("CHANNEL =", CHANNEL)
+print("CHANNELS =", CHANNELS)
 
 class Bot(commands.Bot):
 
@@ -16,7 +23,7 @@ class Bot(commands.Bot):
         super().__init__(
             token=TOKEN,
             prefix="!",
-            initial_channels=[CHANNEL]
+            initial_channels=CHANNELS
         )
 
     async def event_ready(self):
@@ -28,8 +35,9 @@ class Bot(commands.Bot):
 
         texte = message.content
 
+        # Traduction automatique
         try:
-            traduction = GoogleTranslator(source='auto', target='fr').translate(texte)
+            traduction = GoogleTranslator(source="auto", target="fr").translate(texte)
 
             if traduction and traduction.lower() != texte.lower():
                 await message.channel.send(traduction)
@@ -37,6 +45,7 @@ class Bot(commands.Bot):
         except Exception as e:
             print("Erreur traduction :", e)
 
+        # Commande simple
         if texte.lower() == "hello":
             await message.channel.send(f"Salut {message.author.name}")
 
